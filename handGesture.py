6936f5b5da049287b65b2ee70849306844b7d0aa
Vishesh Rawal <vp2005rawal@gmail.com>
2023-07-31 10:34:57 +0530

    Revised hand gestures

--- handGesture.py
+++ handGesture.py
@@ -2,22 +2,18 @@ import cv2
 import mediapipe as mp
 import pyautogui
 
-# Function to detect hand gestures
 def detect_gestures(hand_landmarks):
-    # Check if the index finger is extended (pointing)
+
     index_finger_extended = hand_landmarks.landmark[8].y < hand_landmarks.landmark[6].y
 
-    # Check if the thumb is extended
     thumb_extended = hand_landmarks.landmark[4].x < hand_landmarks.landmark[3].x
 
     return index_finger_extended, thumb_extended
 
-# Main function
 if __name__ == "__main__":
     mp_drawing = mp.solutions.drawing_utils
     mp_hands = mp.solutions.hands
 
-    # Set the width and height of the screen (adjust as per your screen resolution)
     screen_width, screen_height = 1920, 1080
 
     camera = cv2.VideoCapture(0)
@@ -28,21 +24,16 @@ if __name__ == "__main__":
             if not ret:
                 break
 
-            # Convert the frame to RGB (mediapipe works with RGB images)
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            # Process the frame with mediapipe
             results = hands.process(image=frame_rgb)
 
             if results.multi_hand_landmarks:
                 for hand_landmarks in results.multi_hand_landmarks:
-                    # Draw hand landmarks on the frame
                     mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-                    # Detect hand gestures
                     index_finger_extended, thumb_extended = detect_gestures(hand_landmarks)
 
-                    # Change the tab if the index finger is extended and the thumb is not extended
                     if index_finger_extended and not thumb_extended:
                         pyautogui.hotkey('ctrl', 'tab')
 
